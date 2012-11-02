13917bd7739d4915269db1a03b6f0667c5104b16
Kempenhills <support@kempenhills.nl>
2012-11-02 13:37:20 +0100

    Updated readme.md

--- Example project/KHSocialPlugin/Plugins/KHSocialPlugin.m
+++ Example project/KHSocialPlugin/Plugins/KHSocialPlugin.m
@@ -4,13 +4,24 @@
 //
 //  Created by KempenHills on 10/26/12.
 //
+//  Copyright (C) 2012 KempenHills ICT BV
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in
+//  theSoftware without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+//  Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+//  A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+//  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 #import "KHSocialPlugin.h"
 
-#define FB_APP_DISPLAY_NAME     @"Kempenhills Social Plugin"
-#define FB_APP_CAPTION          @"Kempenhills SocialPlugin on GITHub"
-#define FB_APP_DESCRIPTION      @"The KHSocialPlugin makes your life allot easier when it comes to posting stuff to either Facebook or Twitter from a Cordova app!"
+#define FB_APP_DISPLAY_NAME     @"Kempenhills Social Sharing"
+#define FB_APP_CAPTION          @"Kempenhills Social Sharing on GITHub"
+#define FB_APP_DESCRIPTION      @"The Social Sharing plugin makes your life allot easier when it comes to posting stuff to either Facebook or Twitter from a Cordova app!"
 
 @implementation KHSocialPlugin
 
