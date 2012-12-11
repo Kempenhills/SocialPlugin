1c54f4dc51151ee1e09281a5962d4296f816eef9
KempenHills <support@kempenhills.nl>
2012-12-11 15:04:06 +0100

    Android Image support
    It's now possible to share Images on Android (Example can be found in
    the example projects)

--- Native/Android/com/kempenhills/SocialPlugin.java
+++ Native/Android/com/kempenhills/SocialPlugin.java
@@ -12,6 +12,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Intent;
+import android.net.Uri;
 
 import org.apache.cordova.api.Plugin;
 import org.apache.cordova.api.PluginResult;
@@ -23,7 +24,13 @@ public class SocialPlugin extends Plugin {
 		if(action.equals("FBPostToUserTimeline") || action.equals("TWTweetToUserTimeline") || action.equals("PresentActionSheet")) {
 			try {
 				JSONObject jso = new JSONObject(args.getString(0));
-				doSendIntent("Subject", jso.getString("textToShare"));
+				String uri = jso.getString("imageUrl");
+				String text = jso.getString("textToShare");
+				text = (text == null)? "KempenHills Social Plugin": text;
+				if(uri == null)
+					doSendIntent("Subject", text);
+				else
+					doSendIntentImage(text, uri);
 				return new PluginResult(PluginResult.Status.OK);
 			} catch (JSONException e) {
 				return new PluginResult(PluginResult.Status.JSON_EXCEPTION);
@@ -38,7 +45,14 @@ public class SocialPlugin extends Plugin {
 		sendIntent.setType("text/plain");
 		sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
 		sendIntent.putExtra(android.content.Intent.EXTRA_TEXT, text);
-		this.cordova.startActivityForResult(this, Intent.createChooser(sendIntent, null), 0);
+		this.cordova.startActivityForResult(this, Intent.createChooser(sendIntent, text), 0);
 	}
 
+	private void doSendIntentImage(String text, String uri) {
+		Intent share = new Intent(android.content.Intent.ACTION_SEND);
+		share.setType("image/jpeg");
+		share.putExtra(Intent.EXTRA_STREAM, Uri.parse(uri));
+		share.putExtra(android.content.Intent.EXTRA_TEXT, text);
+		this.cordova.startActivityForResult(this, Intent.createChooser(share, text), 0);
+	}
 }
